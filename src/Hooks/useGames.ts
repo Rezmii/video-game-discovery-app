03668b4cb996c393ec63789cca6c67611300f287
Rezmii <bartoszrezmer20@gmail.com>
2024-07-07 14:58:36 +0200

    Refactor entity interfaces

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -2,7 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import ms from "ms";
 import useGameQueryStore from "../store";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 
 const gameApiClient = new APIClient<Game>("/games");
 
--- src/components/GameAttributes.tsx
+++ src/components/GameAttributes.tsx
@@ -1,7 +1,7 @@
 import { Text, SimpleGrid } from "@chakra-ui/react";
 import CriticScore from "./CriticScore";
 import DefinitionItem from "./DefinitionItem";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 
 interface Props {
   game: Game;
